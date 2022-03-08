47ea7119ab5b7849e24944842f709c7af9e92f1d
twni2016 <twni2016@gmail.com>
2022-03-08 18:20:18 -0500

    MINOR

--- envs/pomdp/memory/__init__.py
+++ envs/pomdp/memory/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 delay_fn = lambda runs: (runs - 1) * 7 + 6
 
-for runs in [5, 10, 20, 40]:
+for runs in [1, 2, 5, 10, 20, 40]:
     register(
         f"Catch-{runs}-v0",
         entry_point="envs.pomdp.memory.catch:DelayedCatch",
